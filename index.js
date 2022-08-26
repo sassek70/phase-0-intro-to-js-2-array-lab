31674ac2b17b7b564726b56e1901dcc0b271effb
Kevin Sasse <sassek70@gmail.com>
2022-08-25 21:53:42 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+};
+
+function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+};
+
+function destructivelyRemoveLastCat(cat) {
+    cats.pop(cat);
+};
+
+function destructivelyRemoveFirstCat(cat) {
+    cats.shift(cat);
+};
+
+function appendCat(cat) {
+    const newCat = [...cats, cat]
+    return newCat;
+   };
+
+
+function prependCat(cat) {
+    return [cat, ...cats]
+};
+
+function removeLastCat(cat) {
+    cat = [...cats]
+    cat.pop()
+    return cat;
+   };
+
+function removeFirstCat(cat) {
+    cat = [...cats]
+    cat.shift()
+    return cat;
+};
+
